89bae73321bd8eefcb8fdaf8e664ae1093b237a1
estebanrfp <estebanrfp@gmail.com>
2017-04-05 04:10:27 +0200

    added arrow functions

--- src/scrollify.js
+++ src/scrollify.js
@@ -1,19 +1,16 @@
-export default function Scrolify (hideObject, scrollContent) {
+export default function Scrollify (hideObject, scrollContent) {
   var header = document.querySelector(hideObject)
   var content = document.querySelector(scrollContent)
   var position = content.scrollTop
-  // content.classList.add('header-rotate')
 
-  content.addEventListener('scroll', function (e) {
+  content.addEventListener('scroll', () => {
     let scroll = content.scrollTop
     if (scroll > position) {
       header.classList.remove('header-show')
       header.classList.add('header-hide')
-      // content.classList.remove('header-rotate')
     } else {
       header.classList.remove('header-hide')
       header.classList.add('header-show')
-      // content.classList.add('header-rotate')
     }
     position = scroll
   })
